2b9154ffc83a4993b0a17f668bf33bf9af7f5367
Sam Stiles <sam@phasesolutions.ca>
2013-12-03 17:00:42 -0400

    Adjusted links & about pages, added some structure to custom.js

--- assets/linker/js/custom.js
+++ assets/linker/js/custom.js
@@ -1,30 +1,56 @@
+// Document ready
 jQuery(document).ready(function($) {
 
-  /* --------------------------------------
-      Page-specific functionality
-      PAGE: Contact
-     -------------------------------------- */
+/*
+-----------------------------------------------------------------
+|
+| Page specific functionality
+| Page: /read
+|
+-----------------------------------------------------------------
+*/
 
-  // -----------------------------------------
-      /* Misc non-cosmetic functionality */
-  // -----------------------------------------
+/* Cosmetic */
+// Display the comment form
+$('.post h3').on('click', function(){
+  $('.form-create-comment').slideToggle();
+});
+/* /End cosmetic */
 
-  // -----------------------------------------
-        /* Misc cosmetic functionality */
-  // -----------------------------------------
+/* Non-cosmetic */
+/* /End Non-cosmetic*/
 
-  // Fancy hover toggle on the header image to show the credits
-  $('.top-image').on('mouseenter', function(){
-    $('.top-image-credits').fadeIn(500);
-  });
+/* ---- /Page-specific functionality for page: /read ---- */
 
-  $('.top-image').on('mouseleave', function(){
-    $('.top-image-credits').fadeOut(500);
-  });
+/*
+-----------------------------------------------------------------
+|
+| Non-page-specific non-cosmetic functionality
+|
+-----------------------------------------------------------------
+*/
 
-  // Fancy click toggle of comment form
-  $('.post h3').on('click', function(){
-    $('.form-create-comment').slideToggle();
-  });
+
+
+/* ---- /End non-page-specific non-cosmetic functionality ---- */
+
+/*
+-----------------------------------------------------------------
+|
+| Non-page-specific cosmetic functionality
+|
+-----------------------------------------------------------------
+*/
+
+// Fancy hover toggle on the header image to show the credits
+$('.top-image').on('mouseenter', function(){
+  $('.top-image-credits').fadeIn(500);
+});
+$('.top-image').on('mouseleave', function(){
+  $('.top-image-credits').fadeOut(500);
+});
+
+/* ---- /End non-page-specific non-cosmetic functionality ---- */
 
 });
+// End document ready
